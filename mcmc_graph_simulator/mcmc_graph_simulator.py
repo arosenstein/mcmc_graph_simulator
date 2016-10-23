104171c172002793aa18bbf8696e3f98bec49a5c
Adam Rosenstein <adamrosenstein19@gmail.com>
2016-10-22 21:27:07 -0400

    Completed initial testing for Graph class

--- mcmc_graph_simulator/mcmc_graph_simulator.py
+++ mcmc_graph_simulator/mcmc_graph_simulator.py
@@ -1,1 +1,75 @@
 # -*- coding: utf-8 -*-
+from math import sqrt, e
+import networkx as nx
+import numpy as np
+
+
+class Graph:
+
+	adjacency_matrix = []
+
+
+	def __init__(self, nodes):
+		'''
+		Creates a new connected networkx Graph with the specified nodes 
+	
+		Params
+		------
+		nodes : list of touples
+	
+		'''
+		self.nodes = []
+		self.edges = []
+
+		for node in nodes:
+			self.nodes.append(node)
+
+		for i in range(len(nodes) - 1):
+			self.edges.append((nodes[i], nodes[i+1]))
+
+
+
+	def isConnected(self):
+		'''Determines if the graph is connected
+
+		Parameters
+		-----
+		none
+
+		Return
+		-----
+		isConnected : boolean
+			True if graph is connected, false otherwise
+		'''
+		return True
+
+	def get_distance(self, point1, point2):
+	    '''Calculate distance between two point
+	    
+	    Parameters:
+	        point1: tuple
+	        
+	        point2: tuple    
+	        
+	    Return:
+	        distance: float
+	    '''
+	    
+	    if(len(point1) != len(point2)):
+	        raise ValueError("Points are not of the same dimension")
+	    
+	    distance = 0
+	    
+	    for i in range(len(point1)):
+	        distance += (point1[i] - point2[i])**2
+
+	    return sqrt(distance)
+
+
+
+
+
+
+
+
+
--- tests/test_mcmc_graph_simulator.py
+++ tests/test_mcmc_graph_simulator.py
@@ -14,7 +14,7 @@ import unittest
 from contextlib import contextmanager
 from click.testing import CliRunner
 
-from mcmc_graph_simulator import mcmc_graph_simulator
+from mcmc_graph_simulator.mcmc_graph_simulator import *
 from mcmc_graph_simulator import cli
 
 
@@ -22,14 +22,33 @@ from mcmc_graph_simulator import cli
 class TestMcmc_graph_simulator(unittest.TestCase):
 
     def setUp(self):
+        self.nodes = [(1,2), (3,3), (5,5), (0,0)]
+        self.g = Graph(self.nodes)
         pass
+
+    def test__init__(self):
+        self.assertEqual(len(self.g.nodes), len(self.nodes)) #Graph should have all of the nodes inserted
+        self.assertEqual(len(self.g.edges), len(self.nodes) - 1)  #Graph should have n - 1 edges
+
+    def test_connectedness(self):
+        self.assertTrue(self.g.isConnected())
+
+    def test_distance_calculator(self):
+        point1 = (0,0)
+        point2 = (3,4)
+        self.assertEqual(self.g.get_distance(point1, point2), 5)
+
+        badpoint = (100,0,0)
+        with self.assertRaises(ValueError):
+            self.g.get_distance(point1, badpoint)
+
 
     def tearDown(self):
         pass
 
     def test_000_something(self):
         pass
-
+'''
     def test_command_line_interface(self):
         runner = CliRunner()
         result = runner.invoke(cli.main)
@@ -37,4 +56,4 @@ class TestMcmc_graph_simulator(unittest.TestCase):
         assert 'mcmc_graph_simulator.cli.main' in result.output
         help_result = runner.invoke(cli.main, ['--help'])
         assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+        assert '--help  Show this message and exit.' in help_result.output'''
