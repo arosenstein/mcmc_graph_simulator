38ac8aa2a94beb3dece1573a7092b80749b50af6
Adam Rosenstein <adamrosenstein19@gmail.com>
2016-10-27 00:29:40 -0400

    Removed unnecessary comments

--- tests/test_mcmc_graph_simulator.py
+++ tests/test_mcmc_graph_simulator.py
@@ -203,12 +203,3 @@ class TestMcmc_graph_simulator(unittest.TestCase):
 
     def tearDown(self):
         pass
-'''
-    def test_command_line_interface(self):
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'mcmc_graph_simulator.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output'''
